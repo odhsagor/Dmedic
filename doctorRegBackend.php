82db4563e11d051205f12677b74035607d5a7a4f
obidul huq sagor <72642687+odhsagor@users.noreply.github.com>
2024-06-27 18:17:25 +0600

    Commited By @ODHSAGOR

--- doctorRegBackend.php
+++ doctorRegBackend.php
@@ -1,37 +1,42 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 $servername = "localhost";
 $username = "root";
 $password = ""; 
 $dbname = "Dmedic";
 
-
-$conn = new mysqli("$servername", "$username", "$password", "$dbname");
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 
-$title = $_POST['title'];
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$district = $_POST['district'];
-$national_id = $_POST['national_id'];
-$registration_number = $_POST['registration_number'];
-$doctor_type = $_POST['doctor_type'];
-$mobile_number = $_POST['mobile_number'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+$title = htmlspecialchars($_POST['title']);
+$first_name = htmlspecialchars($_POST['first_name']);
+$last_name = htmlspecialchars($_POST['last_name']);
+$dob = htmlspecialchars($_POST['dob']);
+$gender = htmlspecialchars($_POST['gender']);
+$district = htmlspecialchars($_POST['district']);
+$national_id = htmlspecialchars($_POST['national_id']);
+$registration_number = htmlspecialchars($_POST['registration_number']);
+$doctor_type = htmlspecialchars($_POST['doctor_type']);
+$mobile_number = htmlspecialchars($_POST['mobile_number']);
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$approved = 0; 
 
+$stmt = $conn->prepare("INSERT INTO doctorRegistrations (title, first_name, last_name, dob, gender, district, national_id, registration_number, doctor_type, mobile_number, email, password, approved) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+if ($stmt === false) {
+    die("Error preparing the statement: " . $conn->error);
+}
 
-$stmt = $conn->prepare("INSERT INTO doctorRegistrations(title, first_name, last_name, dob, gender, district, national_id, registration_number, doctor_type, mobile_number, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("ssssssssssss", $title, $first_name, $last_name, $dob, $gender, $district, $national_id, $registration_number, $doctor_type, $mobile_number, $email, $hashed_password);
-
+$stmt->bind_param("ssssssssssssi", $title, $first_name, $last_name, $dob, $gender, $district, $national_id, $registration_number, $doctor_type, $mobile_number, $email, $hashed_password, $approved);
 
 if ($stmt->execute()) {
     echo "New record created successfully";
